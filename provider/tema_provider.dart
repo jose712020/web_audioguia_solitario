daf442bf50f3a6197717b161f5b67a5b5f8b2a44
jose712020 <150953639+jose712020@users.noreply.github.com>
2026-05-26 21:47:28 +0200

    Update tema_provider.dart

--- provider/tema_provider.dart
+++ provider/tema_provider.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TemaProvider extends ChangeNotifier {
+  static const String _claveOscuro = 'isDarkMode';
+
   bool _isDarkMode = false;
+
+  TemaProvider() {
+    _cargarPreferencia();
+  }
 
   bool get isDarkMode => _isDarkMode;
 
-  void toggleDarkMode() {
+  Future<void> _cargarPreferencia() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_claveOscuro) ?? false;
+    notifyListeners();
+  }
+
+  void toggleDarkMode() async {
     _isDarkMode = !_isDarkMode;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_claveOscuro, _isDarkMode);
   }
 }
